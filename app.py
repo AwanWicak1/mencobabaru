9e6b545a7eab1564e1cbf6675cc1199db20b2b64
Awanwicak1 <Awanrjt45@gmail.com>
2023-10-11 13:22:44 +0700

    First Commit

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 MONGODB_URI = os.environ.get("MONGODB_URI")
-DB_NAME =  os.environ.get("DB_NAME")
+DB_NAME = os.environ.get("DB_NAME")
 
 client = MongoClient(MONGODB_URI)
 
@@ -17,9 +17,11 @@ db = client[DB_NAME]
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route("/homework", methods=["POST"])
 def homework_post():
@@ -31,12 +33,14 @@ def homework_post():
         'comment': comment_receive,
     }
     db.fanmessages.insert_one(doc)
-    return jsonify({'msg':'Comment Posted!'})
+    return jsonify({'msg': 'Comment Posted!'})
+
 
 @app.route("/homework", methods=["GET"])
 def homework_get():
     message_list = list(db.fanmessages.find({}, {'_id': False}))
     return jsonify({'messages': message_list})
 
+
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
